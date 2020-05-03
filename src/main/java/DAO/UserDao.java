b3bd4ba0e1afc5da1a73ad74d8df9ccca9395940
Administrator <Administrator@7TSN9WA4XFWYILO>
2020-05-03 16:26:15 +0800

    aaa

--- src/main/java/DAO/UserDao.java
+++ src/main/java/DAO/UserDao.java
@@ -2,6 +2,12 @@ package DAO;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
@@ -20,6 +26,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpRequest;
+
+import java.lang.reflect.Field;
+import com.alibaba.druid.sql.visitor.functions.Concat;
+import com.opensymphony.xwork2.Result;
 
 import MODEL.MailAuthenticator;
 import MODEL.user;
@@ -96,4 +106,64 @@ public class UserDao extends BaseDAO<user> {
         }
     }
 
+    
+    //实现分页
+    public List<user> selectuserpages(user u,int count,int size) throws SQLException, Exception{
+    	Connection connection=Druid().getConnection();
+    	List<user> userlist=new ArrayList<user>();
+    	Field[] fs=u.getClass().getDeclaredFields();
+    	List<Object> valuesList=new ArrayList<Object>();
+    	StringBuffer sqlString=new StringBuffer("select * from user where ");
+    	for(Field f:fs) {
+    		String nameString=f.getName();
+    		f.setAccessible(true);
+
+			// 传递过来的泛型对象t的属性的值
+			Object value = f.get(u);
+			if (value != null && !"".equals(value)) {
+				if ((!value.toString().equalsIgnoreCase("0") && !value.toString().equalsIgnoreCase("0.0"))) {
+					sqlString.append("and ");
+					// 占位符
+					sqlString.append(u.getClass().getSimpleName()).append(".").append(nameString).append("=?");
+					valuesList.add(value);
+				}
+			}
+    	}
+    	//实现分页
+		sqlString.append(" limit ").append((count-1)*size).append(",").append(size);
+		
+    	PreparedStatement ps = connection.prepareStatement(sqlString.toString());
+    	  for (int i = 0; i < valuesList.size(); i++)
+          {
+              ps.setObject(i + 1, valuesList.get(i));
+          }
+    	  ResultSet rs=ps.executeQuery();
+    	   // 元数据对象(里面包含了表头)
+	        ResultSetMetaData rsmd = rs.getMetaData();
+	        
+	        // 此表一共有多少列
+	        int columnCount = rsmd.getColumnCount();
+    	  while(rs.next()) {
+    		     user user=new user();
+ 	            
+ 	            for (int i = 1; i <= columnCount; i++)
+ 	            {
+ 	                // 根据列号 来获得 列名
+ 	                String columnName = rsmd.getColumnName(i);
+ 	                // 根据列名 来获取 当前列的数据
+ 	                Object value = rs.getObject(columnName);
+ 	                // 根据列名 通过反射来找属性对象
+ 	                Field f = user.getClass().getDeclaredField(columnName);
+ 	                if (f != null)
+ 	                {
+ 	                    f.setAccessible(true);
+ 	                    f.set(user, value);
+ 	                }
+ 	                System.out.println(columnName+": "+value);
+ 	            }
+ 	            
+ 	            userlist.add(user);
+    	  }
+		return userlist;
+    }
 }
