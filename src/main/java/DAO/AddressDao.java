48023496a4ff01b63b05fe0fa153bd6bd9ef34d4
Administrator <Administrator@7TSN9WA4XFWYILO>
2020-05-05 22:38:55 +0800

    13131

--- src/main/java/DAO/AddressDao.java
+++ src/main/java/DAO/AddressDao.java
@@ -1,7 +1,19 @@
 package DAO;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import MODEL.address;
 
 public class AddressDao extends BaseDAO {
 
+	public void resetdefaultaddress(address address) throws SQLException, Exception {
+		Connection connection=Druid().getConnection();
+		StringBuffer sqlString=new StringBuffer("update address set isdefault=0 where userid='");
+		sqlString.append(address.getUserid()).append("'");
+		PreparedStatement pStatement=connection.prepareStatement(sqlString.toString());
+		pStatement.executeUpdate();
+	}
 }
--- src/main/java/Services/AddressServices/SetDefaultAddress.java
+++ src/main/java/Services/AddressServices/SetDefaultAddress.java
@@ -1,6 +1,9 @@
 package Services.AddressServices;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import DAO.AddressDao;
+import DAO.UserDao;
 import MODEL.address;
+import MODEL.user;
 
 
 @WebServlet("/SetDefaultAddress")
@@ -20,12 +25,27 @@ public class SetDefaultAddress extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			request.setCharacterEncoding("utf-8");
+			response.setContentType("text/html;charset =UTF-8");
+
+			UserDao userDao=new UserDao();
+
+			user u=userDao.CheckIsLogin(request, response);
+
+			if(u!=null&&"".equals(u.toString())) {
 			AddressDao addressDao=new AddressDao();
 			address oldAddress=new address();
 			address newAddress=new address();
+			//将该用户默认地址重置
+			address tempAddress=new address();
+			tempAddress.setUserid(u.getUserid());
+			addressDao.resetdefaultaddress(tempAddress);
+			
 			oldAddress.setAddressid(request.getParameter("addressid"));
 			newAddress.setIsdefault(request.getParameter("isdefault"));
 			addressDao.updates(newAddress, oldAddress);
+			
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
